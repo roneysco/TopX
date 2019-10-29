60c00fc67b082578e19403ab527d7f08e7852ae2
sergio <sergiobarbosa@usp.br>
2019-10-29 17:26:02 -0300

    Fixes #1

--- correctnessManager.py
+++ correctnessManager.py
@@ -9,15 +9,16 @@ from nltk.tokenize import RegexpTokenizer
 
 
 def Correctness(comment):
-	tokens = tokensManager.GetTokens(comment, 1)
-
-	corr = 0		
-	with open('UserDictionary_pt.txt', encoding="utf8") as list1:
-		dict1 = list1.read().split("\n")
-		for w in tokens:
-			if w in dict1:
-				corr+=1
-			else:
-				pass
-		acc = (corr/len(tokens))*100
-		return acc
+    tokens = tokensManager.GetTokens(comment, 1)
+    corr = 0		
+    if not tokens:  #no tokens found
+        return 0.0
+    with open('UserDictionary_pt.txt', encoding="utf8") as list1:
+        dict1 = list1.read().split("\n")
+        for w in tokens:
+            if w in dict1:
+                    corr+=1
+            else:
+                    pass
+        acc = (corr/len(tokens))*100
+        return acc
